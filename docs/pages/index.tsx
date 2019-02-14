c9cfaabe03ca9e0a7812645ad6a09cd917b22273
Pete Correia <pete@petecorreia.com>
2019-02-14 14:11:21 +0000

    Export Link from Next

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,38 +1,102 @@
 import React from 'react'
-import Link from 'next/link'
-import { Lead, CodeHighlight } from 'tsx-docs'
+import { Lead, CodeHighlight, Link } from 'tsx-docs'
+console.log('Link: ', Link)
 
-export default () => (
+const IndexPage = () => (
 	<>
 		<Lead>
-			Welcome to your Docs{' '}
-			<span role="img" aria-label="party">
-				🎉
-			</span>
+			Minimalist documentation with React Typescript powered by Next.js, Styled
+			Components and Rebass.
 		</Lead>
-
-		<p>
-			We recommend following our{' '}
-			<a href="https://tsx-docs.now.sh/guide">Guide</a> if you're not already
-			doing it.
-		</p>
-
-		<p>
-			You should also have a look at all the{' '}
-			<a href="https://tsx-docs.now.sh/configuration">configuration options</a>{' '}
-			and the provided{' '}
-			<a href="https://tsx-docs.now.sh/components">components</a>.
-		</p>
-
-		<hr />
 
 		<CodeHighlight
 			code={`
+				npm init next-docs
+			`}
+			language="bash"
+		/>
+
+		<h2>Goals</h2>
+
+		<ul>
+			<li>
+				⚛️ <strong>Simplicity</strong> by focusing on React and Typescript
+			</li>
+			<li>
+				💨 <strong>Fast and accessible</strong> effortlessly
+			</li>
+			<li>
+				◾️ <strong>Minimalist</strong> with easy customisation
+			</li>
+		</ul>
+
+		<h2>Getting Started</h2>
+
+		<p>
+			To create a new documentation site, run <code>npm init next-docs</code>{' '}
+			and follow the prompts.
+		</p>
+
+		<p>
+			Follow our{' '}
+			<strong>
+				<Link href="/guide" prefetch>
+					<a>Guide</a>
+				</Link>
+			</strong>
+			.
+		</p>
+		<p>
+			Have a look at{' '}
+			<strong>
+				<Link href="/configuration" prefetch>
+					<a>Configuration</a>
+				</Link>
+			</strong>{' '}
+			options .
+		</p>
+		<p>
+			Use the provided{' '}
+			<strong>
+				<Link href="/components" prefetch>
+					<a>Components</a>
+				</Link>
+			</strong>
+			.
+		</p>
+
+		<h3>Run in development mode</h3>
+
+		<CodeHighlight
+			code={`
+				npm install
 				npm run dev
+			`}
+			language="bash"
+		/>
+
+		<h3>Build for deployment</h3>
+
+		<CodeHighlight
+			code={`
 				npm run build
+				# ...or for static exports use:
+				npm run build-static
+			`}
+			language="bash"
+		/>
+
+		<h3>Type checking</h3>
+
+		<CodeHighlight
+			code={`
 				npm run typecheck
 			`}
 			language="bash"
 		/>
+
+		<p>Enjoy! 🎉</p>
 	</>
 )
+
+export default IndexPage
---
+++ src/components/Link.tsx
@@ -0,0 +1,4 @@
+import Link from 'next/link'
+console.log('NextLink: ', Link)
+
+export { Link }
